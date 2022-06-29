204468ae3f5d18472ac6903f2e5ccc4303951992
Jeff Pitcher <pit19004@byui.edu>
2022-06-28 18:31:21 -0600

    still figuring out first function

--- test.erl
+++ test.erl
@@ -1,9 +1,15 @@
 -module(test). 
--export([add/2,start/0]). 
+% -export([add/2,start/0,hello/0]).
+-compile([export_all]).
 
 add(X,Y) -> 
    Z = X+Y, 
-   io:fwrite("~w~n",[Z]). 
+   io:fwrite("~w~n",[Z]).
+
+hello() ->
+   io:format("hello world-n ").
    
 start() -> 
-   add(5,6).
+   add(5,6),
+   hello().
+
